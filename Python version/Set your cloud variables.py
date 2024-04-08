b4f2be2ce2989bfc1b059a4632155a0216917df9
cedim1 <164538920+cedim1@users.noreply.github.com>
2024-04-08 20:32:17 +0100

    Create Set your cloud variables.py

---
+++ Python version/Set your cloud variables.py
@@ -0,0 +1,2 @@
+connection = session.create_cloud_connection() #Once you have seen your cloud variables enter your project ID
+connection.set_cloud_variable("", "") #the first one is for your cloud variable name, the next is for your value
